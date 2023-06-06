a3c365f274726954e886a8814810faff241bd37e
Nastassia27 <anastasia.derugo@gmail.com>
2023-06-06 19:23:09 +0300

    add image

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,13 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => {
+    return (
+        <div className={s.loader}>
+
+            <img src="Ellipse4.png" alt="Circle 1"/>
+           {/* <img src="Ellipse4-1.png" alt="Circle 2"/>
+            <img src="Ellipse4-2.png" alt="Circle 3"/>
+            <img src="Ellipse4-3.png" alt="Circle 4"/>*/}
+        </div>
+    )
+}
